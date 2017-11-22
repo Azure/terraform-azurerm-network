659c1fb7882a128b7106caabb398fdd42ace4bdb
Ricardo Oliveira <ricardolotr@gmail.com>
2017-11-22 16:42:34 +0000

    remove undefined dns-servers output

--- outputs.tf
+++ outputs.tf
@@ -18,11 +18,6 @@ output "vnet_address_space" {
   value = "${azurerm_virtual_network.vnet.address_space}"
 }
 
-output "vnet_dns_servers" {
-  description = "The DNS servers of the newly created vNet"  
-  value = "${azurerm_virtual_network.vnet.dns_servers}"
-}
-
 output "vnet_subnets" {
   description = "The ids of subnets created inside the newl vNet"    
   value = "${azurerm_subnet.subnet.*.id}"
