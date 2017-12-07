dc51ec9fcc363f78aa8edeb0744b4d160e916bba
Richard Guthrie <rguthriemsft@users.noreply.github.com>
2017-12-06 16:53:59 -0800

    Initial kitchen test. (#9)
    Also fixes `rake build` lint/styling

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "vnet_id" {
-  description = "The id of the newly created vNet"  
-  value = "${azurerm_virtual_network.vnet.id}"
+  description = "The id of the newly created vNet"
+  value       = "${azurerm_virtual_network.vnet.id}"
 }
 
 output "vnet_name" {
-  description = "The Name of the newly created vNet"  
-  value = "${azurerm_virtual_network.vnet.name}"
+  description = "The Name of the newly created vNet"
+  value       = "${azurerm_virtual_network.vnet.name}"
 }
 
 output "vnet_location" {
-  description = "The location of the newly created vNet"    
-  value = "${azurerm_virtual_network.vnet.location}"
+  description = "The location of the newly created vNet"
+  value       = "${azurerm_virtual_network.vnet.location}"
 }
 
 output "vnet_address_space" {
-  description = "The address space of the newly created vNet"    
-  value = "${azurerm_virtual_network.vnet.address_space}"
+  description = "The address space of the newly created vNet"
+  value       = "${azurerm_virtual_network.vnet.address_space}"
 }
 
 output "vnet_subnets" {
-  description = "The ids of subnets created inside the newl vNet"    
-  value = "${azurerm_subnet.subnet.*.id}"
+  description = "The ids of subnets created inside the newl vNet"
+  value       = "${azurerm_subnet.subnet.*.id}"
 }
 
 output "security_group_id" {
-  description = "The id of the security group attached to subnets inside the newly created vNet. Use this id to associate additional network security rules to subnets."    
-  value = "${azurerm_network_security_group.security_group.id}"
+  description = "The id of the security group attached to subnets inside the newly created vNet. Use this id to associate additional network security rules to subnets."
+  value       = "${azurerm_network_security_group.security_group.id}"
 }
