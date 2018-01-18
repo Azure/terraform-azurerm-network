6b76dced286c24b79a80edf2c963482a5300100f
Damien Caro <dcaro@Damiens-MacBook-Pro.local>
2018-01-17 17:06:40 -0800

    removed NSG from module and updaded README

--- main.tf
+++ main.tf
@@ -18,43 +18,7 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name      = "${azurerm_virtual_network.vnet.name}"
   resource_group_name       = "${azurerm_resource_group.network.name}"
   address_prefix            = "${var.subnet_prefixes[count.index]}"
-  network_security_group_id = "${azurerm_network_security_group.security_group.id}"
   count                     = "${length(var.subnet_names)}"
 }
 
-resource "azurerm_network_security_group" "security_group" {
-  name                = "${var.sg_name}"
-  location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.network.name}"
-  tags                = "${var.tags}"
-}
 
-resource "azurerm_network_security_rule" "security_rule_rdp" {
-  count                       = "${var.allow_rdp_traffic ? 1 : 0 }"
-  name                        = "rdp"
-  priority                    = 101
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "3389"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = "${azurerm_resource_group.network.name}"
-  network_security_group_name = "${azurerm_network_security_group.security_group.name}"
-}
-
-resource "azurerm_network_security_rule" "security_rule_ssh" {
-  count                       = "${var.allow_ssh_traffic ? 1 : 0 }"
-  name                        = "ssh"
-  priority                    = 102
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "22"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = "${azurerm_resource_group.network.name}"
-  network_security_group_name = "${azurerm_network_security_group.security_group.name}"
-}
--- outputs.tf
+++ outputs.tf
@@ -23,7 +23,3 @@ output "vnet_subnets" {
   value       = "${azurerm_subnet.subnet.*.id}"
 }
 
-output "security_group_id" {
-  description = "The id of the security group attached to subnets inside the newly created vNet. Use this id to associate additional network security rules to subnets."
-  value       = "${azurerm_network_security_group.security_group.id}"
-}
