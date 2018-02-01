33c07db51fa43741be18439fecc96ac55593db91
Damien Caro <dcaro@Damiens-MacBook-Pro.local>
2018-01-31 20:17:36 -0800

    Correction on fixtures

--- test/integration/default/controls/state_file_spec.rb
+++ test/integration/default/controls/state_file_spec.rb
@@ -8,11 +8,16 @@ control "state_file" do
   # Define how critical this control is.
   impact 0.6
   # The actual test case.
-  describe "the Terraform state file" do
+  describe file("terraform.tfstate.d/kitchen-terraform-default-network/terraform.tfstate") do
+
+    file = File.read("terraform.tfstate.d/kitchen-terraform-default-network/terraform.tfstate")
     # Get json object of terraform state file.
-    subject do json(terraform_state).terraform_version end
+    data_hash = JSON.parse(file)
+    modules = data_hash["modules"]
+
+    subject do modules[1]["resources"]["azurerm_virtual_network.vnet"]["type"] end
 
     # Validate the terraform version number field.
-    it "is accessible" do is_expected.to match /\d+\.\d+\.\d+/ end
+    it "is valid" do is_expected.to match "azurerm_virtual_network" end
   end
 end
