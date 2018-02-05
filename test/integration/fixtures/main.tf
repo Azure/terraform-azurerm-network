db7aa9f7c417eb2d4a16df8efa434392a24921cb
Damien Caro <dcaro@microsoft.com>
2018-02-05 10:38:58 -0800

    Correction on fixtures (#17)

--- test/integration/fixtures/main.tf
+++ test/integration/fixtures/main.tf
@@ -1,6 +1,6 @@
 module "network" {
   source              = "Azure/network/azurerm"
-  resource_group_name = "myapp"
+  resource_group_name = "apptest"
   location            = "westus"
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
