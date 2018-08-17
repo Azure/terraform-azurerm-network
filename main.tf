0336beac0548330b690f3319c31d3cc8a4f71786
Zhenchen Xiao <xiaozhenchenb@126.com>
2018-08-18 02:26:23 +0800

    Add Terratest and Revert to 2.0.0 (#26)
    * Add Terratest and Revert to 2.0.0
    
    * Add tfvars
    
    * Fixed Typos
    
    * Rename Image Name
    
    * Remove Kitchen

--- main.tf
+++ main.tf
@@ -4,13 +4,19 @@ resource "azurerm_resource_group" "network" {
   location = "${var.location}"
 }
 
-module "terraform-azurerm-vnet" {
-  source              = "Azure/vnet/azurerm"
-  version             = "1.0.0"
-  vnet_name           = "${var.vnet_name}"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.vnet_name}"
   location            = "${var.location}"
-  address_space       = "${var.address_space}"
+  address_space       = ["${var.address_space}"]
   resource_group_name = "${azurerm_resource_group.network.name}"
   dns_servers         = "${var.dns_servers}"
   tags                = "${var.tags}"
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.subnet_names[count.index]}"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  resource_group_name  = "${azurerm_resource_group.network.name}"
+  address_prefix       = "${var.subnet_prefixes[count.index]}"
+  count                = "${length(var.subnet_names)}"
 }
--- test/fixture/main.tf
+++ test/fixture/main.tf
@@ -1,7 +1,11 @@
+resource "random_id" "rg_name" {
+  byte_length = 8
+}
+
 module "network" {
-  source              = "../../../"
-  resource_group_name = "apptest"
-  location            = "westus"
+  source              = "../../"
+  resource_group_name = "${random_id.rg_name.hex}"
+  location            = "${var.location}"
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
