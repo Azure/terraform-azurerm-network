e099448e3ae33394d4e90ffd0cd5ad4a82ba96c7
Alex Bevan <AlexBevan@users.noreply.github.com>
2019-12-27 07:36:55 +0000

    fix #30 Tags aren't applied to the resource group
    fix #30

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@
 resource "azurerm_resource_group" "network" {
   name     = var.resource_group_name
   location = var.location
+  tags     = var.tags
+
 }
 
 resource "azurerm_virtual_network" "vnet" {
