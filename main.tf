7f2c81823c74262343ed30a50c89f81868529b50
Alastair Hails <alhails@users.noreply.github.com>
2020-05-25 03:57:27 +0100

    Replace use of deprecated attribute address_prefix (#39)
    Co-authored-by: Alastair Hails <alastairhails@datasoftcomputing.com>

--- main.tf
+++ main.tf
@@ -16,6 +16,6 @@ resource "azurerm_subnet" "subnet" {
   count                = length(var.subnet_names)
   name                 = var.subnet_names[count.index]
   resource_group_name  = data.azurerm_resource_group.network.name
-  address_prefix       = var.subnet_prefixes[count.index]
+  address_prefixes     = [var.subnet_prefixes[count.index]]
   virtual_network_name = azurerm_virtual_network.vnet.name
 }
