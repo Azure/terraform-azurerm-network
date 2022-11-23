c506f86f75a34ad34c2b4437e8076f1f06bf6a00
lonegunmanb <lonegunmanb@hotmail.com>
2022-11-23 17:20:55 +0800

    reorder variables, outputs and arguments (#80)

--- main.tf
+++ main.tf
@@ -10,19 +10,20 @@ locals {
 }
 
 resource "azurerm_virtual_network" "vnet" {
+  address_space       = length(var.address_spaces) == 0 ? [var.address_space] : var.address_spaces
+  location            = local.resource_group_location
   name                = var.vnet_name
   resource_group_name = var.resource_group_name
-  location            = local.resource_group_location
-  address_space       = length(var.address_spaces) == 0 ? [var.address_space] : var.address_spaces
   dns_servers         = var.dns_servers
   tags                = var.tags
 }
 
 resource "azurerm_subnet" "subnet" {
-  count                                          = length(var.subnet_names)
+  count = length(var.subnet_names)
+
+  address_prefixes                               = [var.subnet_prefixes[count.index]]
   name                                           = var.subnet_names[count.index]
   resource_group_name                            = var.resource_group_name
-  address_prefixes                               = [var.subnet_prefixes[count.index]]
   virtual_network_name                           = azurerm_virtual_network.vnet.name
   enforce_private_link_endpoint_network_policies = lookup(var.subnet_enforce_private_link_endpoint_network_policies, var.subnet_names[count.index], false)
   service_endpoints                              = lookup(var.subnet_service_endpoints, var.subnet_names[count.index], [])
