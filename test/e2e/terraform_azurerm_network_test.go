ec00167c6334b7fac65f9dba59fef53cc9594aae
zjhe <hezijie@microsoft.com>
2022-12-08 22:07:43 +0800

    make test cover both count and for_each set

--- test/e2e/terraform_azurerm_network_test.go
+++ test/e2e/terraform_azurerm_network_test.go
@@ -10,8 +10,21 @@ import (
 )
 
 func TestStartup(t *testing.T) {
+	t.Run("for_each", func(t *testing.T) {
+		testStartup(t, true)
+	})
+	t.Run("count", func(t *testing.T) {
+		testStartup(t, false)
+	})
+}
+
+func testStartup(t *testing.T, useForEach bool) {
+	vars := map[string]interface{}{
+		"use_for_each": useForEach,
+	}
 	test_helper.RunE2ETest(t, "../../", "examples/startup", terraform.Options{
 		Upgrade: true,
+		Vars:    vars,
 	}, func(t *testing.T, output test_helper.TerraformOutput) {
 		vnetId, ok := output["test_vnet_id"].(string)
 		assert.True(t, ok)
--- test/upgrade/upgrade_test.go
+++ test/upgrade/upgrade_test.go
@@ -8,6 +8,15 @@ import (
 )
 
 func TestExampleUpgrade_startup(t *testing.T) {
+	t.Run("for_each", func(t *testing.T) {
+		upgradeTest(t, true)
+	})
+	t.Run("count", func(t *testing.T) {
+		upgradeTest(t, false)
+	})
+}
+
+func upgradeTest(t *testing.T, useForEach bool) {
 	currentRoot, err := test_helper.GetCurrentModuleRootPath()
 	if err != nil {
 		t.FailNow()
@@ -16,7 +25,11 @@ func TestExampleUpgrade_startup(t *testing.T) {
 	if err != nil {
 		t.FailNow()
 	}
+	vars := map[string]interface{}{
+		"use_for_each": useForEach,
+	}
 	test_helper.ModuleUpgradeTest(t, "Azure", "terraform-azurerm-network", "examples/startup", currentRoot, terraform.Options{
 		Upgrade: true,
+		Vars:    vars,
 	}, currentMajorVersion)
 }
