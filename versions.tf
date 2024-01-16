68611db6b9cb186efd67dd5ad68065610810edd2
mahdi <frid@MacBook-Pro-de-mahdi.local>
2024-01-16 12:52:03 +0100

    update: remove deprecated params
    update: remove deprecated params

--- versions.tf
+++ versions.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 1.3"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0, < 4.0"
+      version = ">= 3.18.0, < 4.0"
     }
   }
 }
